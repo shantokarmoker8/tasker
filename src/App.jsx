f5789f290e218665776d125059150552d50ca2ef
shantokarmoker8 <shantokarmoker8@gmail.com>
2024-10-16 23:51:09 +0600

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./Header";
+import HeroSection from "./HeroSection";
+import Footer from "./Footer";
 function App() {
   return (
     <>
       <Header></Header>
+      <HeroSection></HeroSection>
+      <Footer></Footer>
     </>
   );
 }
