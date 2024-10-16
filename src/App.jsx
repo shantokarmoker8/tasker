5225ba59167d5966a93a8f616ef16e876d6702c9
shantokarmoker8 <shantokarmoker8@gmail.com>
2024-10-17 00:19:43 +0600

    static

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import TaskBord from "./task/TaskBord";
 import Footer from "./Footer";
 function App() {
   return (
     <>
       <Header></Header>
       <HeroSection></HeroSection>
+      <TaskBord></TaskBord>
       <Footer></Footer>
     </>
   );
